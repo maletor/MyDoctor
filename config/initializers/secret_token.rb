249513ba231af936b762296c8fc05cb1b0164c68
Ellis Berner <eberner@gmail.com>
2010-09-26 21:07:25 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyDoctor::Application.config.secret_token = '37a0b6b45cceee51ddd90ef4045cec114c1f6987f7eb951936c23bd75fb0af9649a4e0e9c74359c9dad4904ec964422129dee107a1d9c80d5bf9005227a9eddc'
